d51c92df4f76e6d11dedd3f22e5d01ceac21c1f9
RamDevops19 <ramdevops17@hotmail.com>
2019-08-27 18:18:36 +0100

    updated1

--- DBUtils.java
+++ DBUtils.java
@@ -1,9 +1,13 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 =======
 UAT branch 1 Updated
 UAT branch
 Stash Command Test
 >>>>>>> d8900b2... update
+=======
+Stash Command Test
+>>>>>>> Stashed changes
 master branch
 Tested
 Update 
